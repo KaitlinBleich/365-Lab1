219ffead141f877d2927456c5dc0e0908f375083
kls551 <32246763+kls551@users.noreply.github.com>
2018-09-24 17:12:34 -0700

    mypartdone

--- schoolSearch.java
+++ schoolSearch.java
@@ -29,9 +29,24 @@ public class schoolsearch {
                 break;
             case "T":
             case "Teacher":
+                for (int i = 0; i < line; i++) {
+                    if (array[1].equals(data[i][6])) {
+                        System.out.println("LastName:\t" + data[i][0]);
+                        System.out.println("FirstName:\t" + data[i][1] + "\n");
+                    }
+                }
                 break;
             case "B":
             case "Bus":
+                for (int i = 0; i < line; i++) {
+                    if (array[1].equals(data[i][4])) {
+                        System.out.println("FirstName:\t" + data[i][1]);
+                        System.out.println("LastName:\t" + data[i][0]);
+                        System.out.println("Grade:\t\t" + data[i][2]);
+                        System.out.println("Classroom:\t" + data[i][3]);
+                        System.out.println();
+                    }
+                }
                 break;
             case "G":
             case "Grade":
@@ -46,17 +61,11 @@ public class schoolsearch {
         }
     }
 
-    private static void search()
-    {
-
-    }
-
     public static void main(String[] args) throws Exception {
         FileReader file = new FileReader("students.txt");
-
         String[][] myData = new String[1000][8];
         BufferedReader bufRead = new BufferedReader(file);
-        String myLine = null;
+        String myLine;
         int line = 0;
 
         // putting it in 2d array
@@ -82,22 +91,6 @@ public class schoolsearch {
             String[] array2 = in.split(": | ");
 
             switchStatement(array2, myData, line);
-
-
-//            // this is student first and bus option.
-//            if((array2[0].equals("S") || array2[0].equals("Student")) && (array3[1].equals("B") || array3[1].equals("Bus")))
-//            {
-//                for (int i = 0; i < line; i++) {
-//                    if (array3[0].equals(myData[i][0])) {
-//                        System.out.println("LastName " + myData[i][0]);
-//                        System.out.println("FirstName " + myData[i][1]);
-//                        System.out.println("BusRoute " + myData[i][4]);
-//                    }
-//                }
-//            }
-//            else {
-//                switchStatement(array2[0], myData, line);
-//            }
         }
     }
 }
